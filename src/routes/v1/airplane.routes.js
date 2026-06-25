cc33b339e1cb16b0287f614dac0f83dc5749cb7a
soni-shivam7544 <sonishivam7544@gmail.com>
2026-06-25 23:58:33 +0530

    Created POST Airplane api service

---
+++ src/routes/v1/airplane.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+const { AirplaneController } = require('../../controllers');
+
+
+// /api/v1/airplanes  POST
+router.post('/', AirplaneController.createAirplane);
+
+module.exports = router;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const { InfoController } = require('../../controllers');
+const airplaneRoutes = require('./airplane.routes');
+
+
+router.use('/airplanes', airplaneRoutes);
 
 router.get('/info', InfoController.info);
 
