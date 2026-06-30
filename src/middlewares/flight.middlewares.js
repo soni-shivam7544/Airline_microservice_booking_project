46e1767ce2c0e40dc379814f332941d9a0fa1e55
soni-shivam7544 <sonishivam7544@gmail.com>
2026-06-30 19:03:06 +0530

    UPDATE: Added a check for departure time smaller than arrival time in flights middleware

--- src/middlewares/flight.middlewares.js
+++ src/middlewares/flight.middlewares.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require('http-status-codes');
 const { ErrorResponse, SuccessResponse } = require('../utils/common');
 const AppError = require('../utils/errors/app.errors');
+const { compareTime } = require('../utils/helpers/datetime-helper');
 
 
 function validateCreateRequest(req, res, next) {
@@ -41,6 +42,12 @@ function validateCreateRequest(req, res, next) {
     }
     if(!req.body.totalSeats) {
         ErrorResponse.error = new AppError( ['Total seats not found in the incoming request body'], StatusCodes.BAD_REQUEST);
+        ErrorResponse.message = 'Something went wrong while creating flight';
+        return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+    }
+
+    if(compareTime(req.body.departureTime, req.body.arrivalTime)){
+        ErrorResponse.error = new AppError( ['Departure time cannot be greater than arrival time'], StatusCodes.BAD_REQUEST);
         ErrorResponse.message = 'Something went wrong while creating flight';
         return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
     }
